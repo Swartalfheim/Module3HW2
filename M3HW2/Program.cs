b67d0957290814735f6b51639dcb0057e0ec39d2
Denys <86122979+Swartalfheim@users.noreply.github.com>
2022-12-08 16:33:38 +0200

    add code

--- M3HW2/Program.cs
+++ M3HW2/Program.cs
@@ -1,10 +1,51 @@
-﻿namespace M3HW2
+﻿using System.Text;
+
+namespace M3HW2
 {
     public class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            IContactCollection<string, PhoneRecord> contactCollection;
+            string culture, previousSection = string.Empty, currentSection;
+
+            Console.OutputEncoding = Encoding.UTF8;
+            Console.WriteLine("Available cultures: ");
+            foreach (string key in ContactCollection.Cultures.Keys)
+            {
+                Console.WriteLine($"{key} ");
+            }
+
+            Console.WriteLine();
+            Console.Write("Enter culture: ");
+            culture = Console.ReadLine();
+            Console.WriteLine();
+            foreach (string key in ContactCollection.Cultures.Keys)
+            {
+                if (key.ToLower().Contains(culture.ToLower()))
+                {
+                    culture = key;
+                    break;
+                }
+            }
+
+            contactCollection = new ContactCollection(new System.Globalization.CultureInfo(culture));
+            foreach (KeyValuePair<string, PhoneRecord> contact in contactCollection)
+            {
+                currentSection = contact.Key;
+                if (previousSection != currentSection)
+                {
+                    if (currentSection != "#")
+                    {
+                        Console.WriteLine();
+                    }
+
+                    Console.WriteLine(currentSection);
+                }
+
+                Console.WriteLine($"{contact.Value.Name.PadRight(15)} {contact.Value.Phone}");
+                previousSection = currentSection;
+            }
         }
     }
 }
