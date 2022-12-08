9cbf70af0b02613885ed132a6bc23c25db09bdf2
Denys <86122979+Swartalfheim@users.noreply.github.com>
2022-12-08 16:17:35 +0200

    add project

---
+++ M3HW2/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace M3HW2
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
